1963721591326348c23c07c6c2f5cb3b69616a6e
noli-noli <gp20a051@oecu.jp>
2023-11-12 15:50:38 +0900

    inputとoutputの言語コードを指定するように修正

--- workspace/post.py
+++ workspace/post.py
@@ -2,29 +2,36 @@ import requests
 
 
 #サーバURL
-url = "http://133.89.44.4:"
+url = "http://133.89.44.20:"
 
 #ポート番号
 port = "49152"
 
-#言語コード
-lang_code = "eng_Latn"
+#翻訳元の言語コード
+input_code = "jpn_Jpan"
 
-#最大生成トークン数
-max_length = 10000
+#翻訳先の言語コード
+output_code = "eng_Latn"
+
+#最大生成トークン数(MAX=1000がおススメ)
+max_length = 2000
 
 
 
 def main():
-    text = input("Please input text: ")
-    if text == "":
-        text = """昨今の大規模言語モデルの進化は凄まじく早い。約1ヶ月毎に新しいモデルが公開されている。また、それらを動かす為の条件も非常に厳しく定義されており、最新のGPGPUが無いと動作不可能等ざらである。"""
+    #text = input("Please input text: ")
+    #if text == "":
+    text = """一気に寒くなった。
+布団が私を離してくれない。"""
     #textに翻訳したい文章を入れる
     try:
-        response = requests.get(url + port + "/items/?text=" + text + "&lange_code=" + lang_code + "&max_length=" + str(max_length))
+        response = requests.get(url + port + "/items/?text=" + text + "&input_code=" + input_code + "&output_code=" + output_code + "&max_length=" + str(max_length) )
+        print("input text:" + text.replace("\n",""))
         print("response text: " + response.text)
     except Exception as e:
         print("Error: ", e)
+
+
 
 if __name__ == "__main__":
     main()
