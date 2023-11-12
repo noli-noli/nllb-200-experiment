c7020a2e84c58d0210f87a7829b915424a8293d6
noli-noli <gp20a051@oecu.jp>
2023-11-12 15:51:36 +0900

    inputとoutputを受け取る形に修正

--- workspace/nllb-fastapi.py
+++ workspace/nllb-fastapi.py
@@ -10,8 +10,7 @@ app = FastAPI()
 
 custom_headers = {"Access-Control-Allow-Origin": "*",}
 
-tokenizer = AutoTokenizer.from_pretrained("models/nllb-200-distilled-600M",src_lang="eng_Latn")
-model = AutoModelForSeq2SeqLM.from_pretrained("models/nllb-200-distilled-600M")
+model = AutoModelForSeq2SeqLM.from_pretrained("models/nllb-200-3.3B")
 
 if torch.cuda.is_available():
     print("Using GPU")
@@ -21,15 +20,16 @@ else:
     model = model.to("cpu")
 
 @app.get("/items/")
-def run_model(text:str, lange_code:str,max_length:int):
-    print(f"lange_code: {lange_code}")
+def run_model(text:str,input_code:str,output_code:str,max_length:int):
+    print(f"output_code: {output_code}")
     print(f"text: {text}")
+    tokenizer = AutoTokenizer.from_pretrained("models/nllb-200-3.3B",src_lang=input_code)
     input = tokenizer(text.replace("\n",""), return_tensors="pt")
     with torch.no_grad():
         output_ids = model.generate(
             **input.to(model.device),
-            forced_bos_token_id=tokenizer.lang_code_to_id[lange_code],
+            forced_bos_token_id=tokenizer.lang_code_to_id[output_code],
             max_length=max_length
-        )
-    result = tokenizer.decode(output_ids.tolist()[0]).replace(f"{lange_code}", "").replace("</s>", "").replace(".", "。").replace(",", "、")
+        )   
+    result = tokenizer.decode(output_ids.tolist()[0]).replace(f"{output_code}", "").replace("</s>", "")
     return JSONResponse(content=result, headers=custom_headers)
